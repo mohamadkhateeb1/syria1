52f89182f21b919951f3950eda6874f547d20ab8
mohamadkhateeb1 <mohamadkhateeb46@gmail.com>
2025-11-25 12:38:54 +0200

    Syria

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,8 +29,7 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-        return redirect()->intended(route('dashboard', absolute: false));
-    }
+   return redirect()->intended(route('categories.index', absolute: false));   }
 
     /**
      * Destroy an authenticated session.
