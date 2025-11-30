4fcb93bf3630ed6caaafc9b50339c324585c74ac
mohamadkhateeb1 <mohamadkhateeb46@gmail.com>
2025-11-30 15:10:50 +0200

    Syria

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,20 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\CategoriesController;
 use App\Http\Controllers\Dashboard\DashboardController;
 
-Route::group(
-  [
-    'prefix' => '/dashboard',
- 'as' => 'dashboard.',   
-  ],
-  function () {
-    // Route::get('/index',[DashboardController::class,'index'])->name('dashboard.index');
-    // Route::get('categories', [App\Http\Controllers\Dashboard\CategoriesController::class, 'index'])->name('categories.index');
-    // Route::get('/categories/create', [App\Http\Controllers\Dashboard\CategoriesController::class, 'create'])->name('categories.create');
-    // Route::post('/categories/store', [App\Http\Controllers\Dashboard\CategoriesController::class, 'store'])->name('categories.store');
-    // Route::get('/categories/{id}/edit', [App\Http\Controllers\Dashboard\CategoriesController::class, 'edit'])->name('categories.edit');
-    // Route::put('/categories/{id}/update', [App\Http\Controllers\Dashboard\CategoriesController::class, 'update'])->name('categories.update');
-    // Route::delete('/categories/{id}', [App\Http\Controllers\Dashboard\CategoriesController::class, 'destroy'])->name('categories.destroy');
-    //  Route::resource('categories', App\Http\Controllers\Dashboard\CategoriesController::class);
-    Route::resource('categories', App\Http\Controllers\Dashboard\CategoriesController::class);
-  }
-);
+
+    Route::get('/categories', [App\Http\Controllers\Dashboard\CategoriesController::class, 'index'])->name('categories.index');
+    Route::get('/categories/create', [App\Http\Controllers\Dashboard\CategoriesController::class, 'create'])->name('categories.create');
+    Route::post('/categories/store', [App\Http\Controllers\Dashboard\CategoriesController::class, 'store'])->name('categories.store');
+    Route::get('/categories/{id}/edit', [App\Http\Controllers\Dashboard\CategoriesController::class, 'edit'])->name('categories.edit');
+    Route::put('/categories/{id}/update', [App\Http\Controllers\Dashboard\CategoriesController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{id}', [App\Http\Controllers\Dashboard\CategoriesController::class, 'destroy'])->name('categories.destroy');
