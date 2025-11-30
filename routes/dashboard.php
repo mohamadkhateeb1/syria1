0f15eb5269fa7798dee2ca68f6164144ad1f9ad3
mohamadkhateeb1 <mohamadkhateeb46@gmail.com>
2025-11-30 16:28:04 +0200

    Syria

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,11 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\CategoriesController;
 use App\Http\Controllers\Dashboard\DashboardController;
-
-
-    Route::get('/categories', [App\Http\Controllers\Dashboard\CategoriesController::class, 'index'])->name('categories.index');
-    Route::get('/categories/create', [App\Http\Controllers\Dashboard\CategoriesController::class, 'create'])->name('categories.create');
-    Route::post('/categories/store', [App\Http\Controllers\Dashboard\CategoriesController::class, 'store'])->name('categories.store');
-    Route::get('/categories/{id}/edit', [App\Http\Controllers\Dashboard\CategoriesController::class, 'edit'])->name('categories.edit');
-    Route::put('/categories/{id}/update', [App\Http\Controllers\Dashboard\CategoriesController::class, 'update'])->name('categories.update');
-    Route::delete('/categories/{id}', [App\Http\Controllers\Dashboard\CategoriesController::class, 'destroy'])->name('categories.destroy');
+    Route::get('categories/index', [CategoriesController::class, 'index'])->name('categories.index');
+    Route::get('/categories/create', [CategoriesController::class, 'create'])->name('categories.create');
+    Route::post('/categories/store', [CategoriesController::class, 'store'])->name('categories.store');
+    Route::get('/categories/{id}/edit', [CategoriesController::class, 'edit'])->name('categories.edit');
+    Route::put('/categories/{id}/update', [CategoriesController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{id}', [CategoriesController::class, 'destroy'])->name('categories.destroy');
+?>
